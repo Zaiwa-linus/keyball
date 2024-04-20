dfc6db4d2beb54a6caabd3d789d287a61d78a9ae
zaiwa <35264289+Zaiwa-linus@users.noreply.github.com>
2024-04-20 21:27:05 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -37,3 +37,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 3
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
