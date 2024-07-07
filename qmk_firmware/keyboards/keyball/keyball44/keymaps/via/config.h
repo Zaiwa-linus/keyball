04060991d8770ef927873d4b4491d1981c789419
zaiwa <35264289+Zaiwa-linus@users.noreply.github.com>
2024-07-07 14:09:24 +0900

    Update config.h
    TT Holdを120に設定

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -33,6 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
+#define TAPPING_TERM 120
+
 #define TAP_CODE_DELAY 5
 #define TAPPING_FORCE_HOLD
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
